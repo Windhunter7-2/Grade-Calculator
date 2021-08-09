6acb1c2ba064536c179c39c72b13f658623c6ef4
Nicholas Szymonik <nicholascoles.a@gmail.com>
2021-08-09 15:16:57 -0400

    Algorithm: wrote addKnown, created framework for testing.

--- src/Math/Subgrade.java
+++ src/Math/Subgrade.java
@@ -14,6 +14,22 @@ public class Subgrade {
 	
 	public boolean isExpected;
 
+	/**
+	 * Constructors for testing purposes only.
+	 */
+	public Subgrade(boolean toCalculate, double grade, double expectedGrade, String name) {
+		this.toCalculate = toCalculate;
+		this.grade = grade;
+		this.expectedGrade = expectedGrade;
+		this.name = name;
+		this.isEmpty = (grade == -1.0);
+		this.isExpected = (expectedGrade != -1.0);
+	}
+	
+	public Subgrade(double grade) {
+		this(false, grade, -1.0, "");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
